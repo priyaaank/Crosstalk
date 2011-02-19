026b7fe81861ba26af5c3cb09481bb640660331b
Priyank <priyaaank@gmail.com>
2011-02-19 14:14:25 +0530

    Added project skeleton

---
+++ src/com/barefoot/crosstalk/Crosstalk.java
@@ -0,0 +1,13 @@
+package com.barefoot.crosstalk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Crosstalk extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
