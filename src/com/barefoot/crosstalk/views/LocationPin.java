4f910f7bc634bc0c39404d70c36861eecb8b5713
Priyank <priyaaank@gmail.com>
2011-04-01 15:55:49 +0530

    Moved this location pin file correctly. Missed this earlier.

---
+++ src/com/barefoot/crosstalk/views/LocationPin.java
@@ -0,0 +1,47 @@
+package com.barefoot.crosstalk.views;
+
+import android.graphics.Point;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapView;
+import com.google.android.maps.OverlayItem;
+
+public class LocationPin extends OverlayItem {
+	
+	private ImageView dragImage=null;
+	private int xDragImageOffset=0;
+	private int yDragImageOffset=0;
+	private int xDragTouchOffset=0;
+	private int yDragTouchOffset=0;
+
+	public LocationPin(GeoPoint point, String title, String snippet, ImageView impersonaterImage) {
+		super(point, title, snippet);
+		this.dragImage = impersonaterImage;
+		xDragImageOffset=dragImage.getDrawable().getIntrinsicWidth()/2;
+		yDragImageOffset=dragImage.getDrawable().getIntrinsicHeight();
+	}
+	
+	protected void swapMeWithImpersonater(final int x, final int y, final MapView map, final Point p) {
+		xDragTouchOffset=0;
+		yDragTouchOffset=0;
+
+		setImpersonaterPosition(p.x, p.y);
+		xDragTouchOffset=x-p.x;
+		yDragTouchOffset=y-p.y;
+	}
+	
+	protected LocationPin swapImpersonaterWithMe(final int x, final int y, final MapView map) {
+		dragImage.setVisibility(View.GONE);
+		GeoPoint pointToDropAt = map.getProjection().fromPixels(x-xDragTouchOffset, y-yDragTouchOffset);
+		return new LocationPin(pointToDropAt, this.getTitle(), this.getSnippet(), dragImage);
+	}
+
+	protected void setImpersonaterPosition(final int x, final int y) {
+		RelativeLayout.LayoutParams lp= (RelativeLayout.LayoutParams)dragImage.getLayoutParams();
+		lp.setMargins(x-xDragImageOffset-xDragTouchOffset, y-yDragImageOffset-yDragTouchOffset, 0, 0);
+		dragImage.setLayoutParams(lp);
+	}
+}
